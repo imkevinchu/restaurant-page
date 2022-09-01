00873c17358ac8f2632aa7ec419e90b247ce1073
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-08-31 19:33:08 -0700

    insert content

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ const createContactPage = () => {
   const contactPage = document.createElement("div");
   contactPage.classList.add("contact");
 
+  const phone = document.createElement("p");
+  phone.textContent = "(310) 867-5309";
+
+  const address = document.createElement("p");
+  address.textContent = "585 Venice Blvd, Venice, CA 90291";
+
+  contactPage.appendChild(phone);
+  contactPage.appendChild(address);
+
   return contactPage;
 };
 
@@ -44,9 +53,12 @@ const createHomePage = () => {
   const homePage = document.createElement("div");
   homePage.classList.add("home");
 
-  const homePageTest = document.createElement("p");
-  homePageTest.textContent = "loaded home page";
-  homePage.appendChild(homePageTest);
+  const heading = document.createElement("h2");
+  const subtitle = document.createElement("p");
+  heading.textContent = "Quality that doesn't go to waste";
+  subtitle.textContent = "Since 1989";
+  homePage.appendChild(heading);
+  homePage.appendChild(subtitle);
 
   return homePage;
 };
@@ -72,11 +84,41 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+// import Hamburger from "./img/hamburger.jpg";
+// import Cheeseburger from "./img/cheeseburger.jpg";
+// import Fries from "./img/fries.jpg";
+// import Milkshake from "./img/milkshake.jpg";
+
 const createMenuPage = () => {
   const menuPage = document.createElement("div");
   menuPage.classList.add("menu");
 
+  menuPage.appendChild(createMenuItem("Hamburger", "600"));
+  menuPage.appendChild(createMenuItem("Cheeseburger", "400"));
+  menuPage.appendChild(createMenuItem("Fries", "200"));
+  menuPage.appendChild(createMenuItem("Milkshake", "500"));
+
   return menuPage;
+};
+
+const createMenuItem = (name, cal) => {
+  const menuItem = document.createElement("div");
+  menuItem.classList.add("menu-item");
+
+  const title = document.createElement("h3");
+  const calories = document.createElement("p");
+  title.textContent = name;
+  calories.textContent = cal;
+
+  //   const img = document.createElement("img");
+  //   img.src = `${name.toUpperCase()}`;
+  //   img.alt = `${name}`;
+
+  menuItem.appendChild(title);
+  menuItem.appendChild(calories);
+  //   menuItem.appendChild(img);
+
+  return menuItem;
 };
 
 const loadMenuPage = () => {
@@ -106,6 +148,21 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+
+const createHeader = () => {
+  const header = document.createElement("header");
+  header.classList.add("header");
+  header.setAttribute("id", "header");
+
+  const title = document.createElement("h1");
+  title.classList.add("title");
+  title.textContent = "Out-N-In Burger";
+
+  header.appendChild(title);
+  header.appendChild(createNav());
+
+  return header;
+};
 
 const createNav = () => {
   const nav = document.createElement("nav");
@@ -167,7 +224,7 @@ const setBtn = (btn) => {
 const loadWebsite = () => {
   const content = document.getElementById("content");
 
-  content.appendChild(createNav());
+  content.appendChild(createHeader());
   content.appendChild(createMain());
 
   (0,_home__WEBPACK_IMPORTED_MODULE_0__["default"])();
